1485e43232c09ea8650b93e68c5e2a059a998b07
Michael Kapronica <mkap25@yahoo.com>
2019-08-20 03:13:25 -0400

    Add files via upload

---
+++ Stringnames.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            var firstName = "Mike";
+            String lastName = "Kap";
+
+            var fullName = firstName + " " + lastName;
+
+            var myFullName= string.Format("My name is {0} {1}", firstName, lastName);
+            var names = new string[3] { "John", "Jack", "Mary" };
+            var formattedNames = string.Join(",", names);
+            Console.WriteLine(formattedNames);
+
+            var text = 
+            
+        }
+    }
+}
